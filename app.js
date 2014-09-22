a2b546fc0d287cfa36e05a77c0092c80c99d2c84
Jonathan Blake <jonawayneblake@gmail.com>
2014-09-22 17:49:25 -0400

    added logo

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
